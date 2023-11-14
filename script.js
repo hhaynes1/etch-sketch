be211232331a0fc596704fe778cf5ae4b41d2ccf
hhaynes1 <94259379+hhaynes1@users.noreply.github.com>
2023-11-14 13:58:35 -0500

    Add grid size button
    Add grid size button that changes number of cells per side.
    Grid size locked at 960x960, cells adjust width/height to fit in space.
    Add text displaying current number of cells per side.

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const body = document.querySelector('body');
-const container = document.querySelector('.container');
+const container = document.getElementById('container');
 body.appendChild(container);
 const resetBtn = document.querySelector('.reset');
 const gridSizeBtn = document.querySelector('.grid-size');
-const cellSizeBtn = document.querySelector('.cell-size');
-const removeBtn = document.querySelector('.remove'); // test
+const numCellsText = document.querySelector('.cell-count');
 
-let cellSize = 40;
-let gridRows = 16;
-let gridColumns = 16;
+// default grid size
+let cellSize = 60;
+let gridSides = 16;
+let containerSize = container.clientWidth;
 
+// set all cells to black
 resetBtn.addEventListener('click', () => {
     const cells = [...document.querySelectorAll('.cell')];
     for (let i = 0; i < cells.length; i++) {
@@ -17,44 +18,31 @@ resetBtn.addEventListener('click', () => {
     }
 })
 
-// gridSizeBtn.addEventListener('click', () => {
-//     gridRows = +prompt('Enter number of rows: ', 16);
-//     gridColumns = +prompt('Enter number of columns: ', 16);
-//     console.log('gridrows: ' + gridRows);
-//     console.log('gridcols: ' + gridColumns);
-//     drawGrid(gridRows, gridColumns);
-// })
+// button to change number of grid sides
+gridSizeBtn.addEventListener('click', () => {
+    gridSides = +prompt('Enter number of grid sides: ', 16);
+    drawGrid(gridSides);
+})
 
-// cellSizeBtn.addEventListener('click', () => {
-//     let size = +prompt('Enter cell size: ', 40);
-//     cellSize = size + 'px';
-//     console.log(cellSize + ', ' + typeof cellSize);
-// })
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
 
-// removeBtn.addEventListener('click', () => {
-//     clearGrid();
-// })
+function drawGrid(sides) {
+    clearGrid();
+    container.style.gridTemplateRows = `repeat(${sides}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${sides}, 1fr)`;
 
-// function clearGrid() {
-//     while (container.firstChild) {
-//         container.removeChild(container.firstChild);
-//     }
-//     // change div container size
-//     container.style.width = '0px';
-//     container.style.height = '0px';
-//     // also change border size
-//     container.border = '0px';
-// }
-
-function drawGrid(rows, columns) {
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
+    for (let i = 0; i < sides; i++) {
+        for (let j = 0; j < sides; j++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
             container.appendChild(cell);
 
             cell.addEventListener('mouseover', () => {
-                cell.style.backgroundColor = 'blue';
+                cell.style.backgroundColor = 'gray';
             })
 
             cell.addEventListener('dblclick', () => {
@@ -62,6 +50,10 @@ function drawGrid(rows, columns) {
             })
         }
     }
+
+    cellSize = containerSize / gridSides;
+    numCellsText.textContent = `${gridSides}`;
 }
 
-drawGrid(gridRows, gridColumns);
+// initialize
+drawGrid(gridSides);
